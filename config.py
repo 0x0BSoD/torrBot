d5b2d70bc15124b1bf92502152f5373c1ae6013a
0x0BSoD <zlodey23@gmail.com>
2017-12-08 23:42:42 +0300

    Refactoring

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 token = "You_Telegram_Bot_Token"
 partition = "Partition_For_DF"
+verify = False
+# dirs
 downloadsDir = "Downloads dir"
 torrentsDir = "watch dir for transmission daemon"
+sensorsTarget = ["sensors"]
 # for transmission
 url = "http[s]://yot_ip:9091/transmission/rpc"
 username = "username"
--- main.py
+++ main.py
@@ -1,42 +1,20 @@
 # _*_ coding: utf-8 _*_
 import telebot
-import json
-import requests
+import time
 
-from pathlib import Path
-from modules.logger import log
-from modules.router import parse
-from modules import strings as cs
+from utils.logger import log
+from utils.router import parse
+from utils import strings as cs
+from interface.keyboards import kbd
 import config as cfg
 
 bot = telebot.TeleBot(cfg.token)
 print(bot.get_me())
-
 errVideo = open('./media/error.mp4', 'rb')
 
-
-@bot.message_handler(commands=['start', 'back'])
-def handle_start(message):
-    userMarkup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True,
-                                                   one_time_keyboard=False)
-    userMarkup.row('/torrent_сontrols')
-    userMarkup.row('chmod 777', 'Dir Content')
-    userMarkup.row('CPU', 'Free Space')
-    bot.send_message(message.from_user.id,
-                     cs.helloText,
-                     reply_markup=userMarkup)
-
-
-@bot.message_handler(commands=['torrent_сontrols'])
-def handle_start(message):
-    userMarkup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True,
-                                                   one_time_keyboard=False)
-    userMarkup.row('Start All', 'Stop All')
-    userMarkup.row('Activity', 'Ratio')
-    userMarkup.row('/back')
-    bot.send_message(message.from_user.id,
-                     cs.helloText,
-                     reply_markup=userMarkup)
+# Keyboarads
+kbd.torrKbd(bot)
+kbd.serverKbd(bot)
 
 
 @bot.message_handler(commands=['help'])
@@ -72,16 +50,25 @@ def echo_all(message):
             with open(path, 'wb') as new_file:
                 new_file.write(downloaded_file)
             log(message, 'Added!')
-            bot.send_message(message.chat.id, "<b>{} Added!</b>".format, parse_mode="html")
+            bot.send_message(message.chat.id, "{} == Added!".format(name))
         else:
             log(message, 'Errored!')
             bot.send_message(message.chat.id, 'It\'s not torrent file!')
             bot.send_video(message.chat.id, errVideo)
     except Exception as e:
             log(message, 'Errored!')
-            bot.send_message(message.chat.id, 'Some Fucking Error ( ⚆ _ ⚆ )')
+            bot.send_message(message.chat.id,
+                             'Some Fucking Error ( ⚆ _ ⚆ ) {}'.format(e),
+                             parse_mode="html")
             bot.send_video(message.chat.id, errVideo)
 
 
-bot.polling(none_stop=True, interval=0)
-errVideo.close()
+def main_loop():
+    bot.polling(True)
+    while 1:
+        time.sleep(3)
+
+
+if __name__ == '__main__':
+    main_loop()
+    errVideo.close()
